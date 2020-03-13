ccbd038f2b0c460924ac40b94ecff47571cac790
kaylea976 <kaylea.stewart@selu.edu>
2020-03-13 13:43:23 -0500

    Update project 1.py

--- project 1.py
+++ project 1.py
@@ -1,3 +1,4 @@
+#project from cmps 329 implementing TCP protocol
 Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
 >>> import socket
